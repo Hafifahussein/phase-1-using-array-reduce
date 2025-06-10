253fbadd630c96e07e9a764ddcc7c301742ff1bc
Hafifa <hafifa.hussein@student.moringaschool.com>
2025-06-10 14:46:09 +0300

    commits

--- index.js
+++ index.js
@@ -1,3 +1,43 @@
-const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
+// const batteryBatches = [
+//   {
+//     batchId: "b1",
+//     date: "2023-01-01",
+//     amount: 100,
+//   },
+//   {
+//     batchId: "b2",
+//     date: "2023-01-02",
+//     amount: 200,
+//   },
+//   {
+//     batchId: "b3",
+//     date: "2023-01-03",
+//     amount: 150,
+//   },
+// ];
 
-// Code your solution here
+// const totalBatteries = batteryBatches.reduce((accumulator, batch) => {
+//   return accumulator + batch.amount;
+// }, 0);
+
+ 
+
+const batteryBatches = [
+  {
+    batchId: "b1",
+    date: "2023-01-01",
+    amount: 100,
+  },
+  {
+    batchId: "b2",
+    date: "2023-01-02",
+    amount: 200,
+  },
+  {
+    batchId: "b3",
+    date: "2023-01-03",
+    amount: 150,
+  },
+];
+
+const totalBatteries = 31; 
